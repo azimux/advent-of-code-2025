225f2d27a5feab1af55722e35333058ce32b6d7f
Miles Georgi <azimux@gmail.com>
2025-12-24 11:23:56 -0800

    Lots of debugging but still getting the wrong answer on the test data

--- d10/p2/machine.rb
+++ d10/p2/machine.rb
@@ -9,17 +9,20 @@ class Machine
   end
 
   def minimum_pushes_required
+    binding.pry
+
     target_button = button_with_most_joltage_indices
     return 0 if target_button.nil?
 
     target_joltage_index = minimum_nonzero_joltage_index(target_button)
     return 0 if target_joltage_index.nil?
 
+    binding.pry
+
     target_joltage = joltages[target_joltage_index]
-    binding.pry if target_joltage.zero?
+
     relevant_buttons = buttons.select { |button| button.include?(target_joltage_index) }
     relevant_buttons.reject! do |button|
-      binding.pry if button.nil?
       button.joltages_to_increment.any? do |joltage_index|
         joltages[joltage_index].zero?
       end
@@ -43,6 +46,8 @@ class Machine
       end
     end
 
+    binding.pry
+
     unless minimum_submachine_pushes.nil?
       target_joltage + minimum_submachine_pushes
     end
@@ -57,19 +62,21 @@ class Machine
   end
 
   def minimum_nonzero_joltage_index(button)
-    binding.pry if button.nil?
     joltages_to_increment = button.joltages_to_increment
 
     min_index = nil
     min_joltage = nil
 
-    (0...joltages_to_increment.size).each do |joltage_index|
+    (0...joltages_to_increment.size).each do |i|
+      joltage_index = joltages_to_increment[i]
       value = joltages[joltage_index]
 
       if value.positive? && (min_joltage.nil? || value < min_joltage)
         min_joltage = value
         min_index = joltage_index
       end
+    end.tap do
+      binding.pry if it.nil?
     end
 
     min_index
