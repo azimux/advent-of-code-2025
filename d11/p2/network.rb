8c3ef8f2b25d05f2d6ddcadd0fb1e15357ab5c2a
Miles Georgi <azimux@gmail.com>
2026-03-20 15:05:50 -0700

    Split out NetworkPathCounter class from Network

--- d11/p2/network.rb
+++ d11/p2/network.rb
@@ -1,22 +1,12 @@
 require_relative "cache_thread"
 
 class Network
-  attr_accessor :graph,
-                :start_at,
-                :end_at,
-                :seen,
-                :cache,
-                :inputs_file,
-                :cache_thread,
-                :wait_on_cache
+  attr_accessor :graph, :inputs_file_name
 
-  def initialize(inputs_file, start_at:, end_at:, wait_on_cache: false)
-    self.inputs_file = inputs_file
-    self.start_at = start_at
-    self.end_at = end_at
-    self.wait_on_cache = wait_on_cache
+  def initialize(inputs_file_name)
+    self.inputs_file_name = inputs_file_name
 
-    inputs = File.read(inputs_file)
+    inputs = File.read(inputs_file_name)
 
     lines = inputs.split("\n")
 
@@ -34,77 +24,5 @@ class Network
     end
   end
 
-  def answer
-    self.seen = Set.new
-    self.cache = CacheThread.load_cache(inputs_file, start_at:, end_at:)
-    self.cache_thread = CacheThread.new(inputs_file, cache, start_at:, end_at:)
-
-    path_count_from(start_at, seen_fft: false, seen_dac: false, path: nil)
-  ensure
-    self.seen = nil
-
-    if wait_on_cache
-      cache_thread.stop
-    else
-      cache_thread.close!
-    end
-  end
-
-  def join = cache_thread.join
-
-  private
-
-  def cached(path)
-    result = cache[path]
-
-    if result
-      puts "Cache hit on #{path}: #{result}"
-    end
-
-    unless result
-      result = yield
-
-      cache[path] = result
-
-      cache_thread.write_to_cache(path, result)
-    end
-
-    result
-  end
-
-  def path_count_from(node, seen_fft:, seen_dac:, path:)
-    path = NetworkPath.new(node, path)
-
-    return 0 if seen.include?(path)
-
-    seen << path
-
-    cached(path) do
-      case node
-      when :fft
-        seen_fft = true
-      when :dac
-        seen_dac = true
-      end
-
-      destinations = graph[node]
-
-      if destinations.nil?
-        require "pry"
-        binding.pry
-      end
-      destinations.map do |destination|
-        if destination == end_at
-          if seen_fft && seen_dac
-            puts "found a path!!"
-            1
-          else
-            0
-          end
-        else
-          path_count_from(destination, seen_fft:, seen_dac:, path:)
-        end
-      end.sum
-    end
-  end
+  def [](key) = graph[key]
 end
---
+++ d11/p2/network_path_counter.rb
@@ -0,0 +1,89 @@
+require_relative "cache_thread"
+
+class NetworkPathCounter
+  attr_accessor :network,
+                :start_at,
+                :end_at,
+                :seen,
+                :cache,
+                :cache_thread,
+                :wait_on_cache
+
+  def initialize(network, start_at:, end_at:, wait_on_cache: false)
+    self.network = network
+    self.start_at = start_at
+    self.end_at = end_at
+    self.wait_on_cache = wait_on_cache
+  end
+
+  def answer
+    self.seen = Set.new
+    self.cache = CacheThread.load_cache(inputs_file_name, start_at:, end_at:)
+    self.cache_thread = CacheThread.new(inputs_file_name, cache, start_at:, end_at:)
+
+    path_count_from(start_at, seen_fft: false, seen_dac: false, path: nil)
+  ensure
+    self.seen = nil
+
+    if wait_on_cache
+      cache_thread.stop
+    else
+      cache_thread.close!
+    end
+  end
+
+  def inputs_file_name = network.inputs_file_name
+  def join = cache_thread.join
+
+  private
+
+  def cached(path)
+    result = cache[path]
+
+    if result
+      puts "Cache hit on #{path}: #{result}"
+    end
+
+    unless result
+      result = yield
+
+      cache[path] = result
+
+      cache_thread.write_to_cache(path, result)
+    end
+
+    result
+  end
+
+  def path_count_from(node, seen_fft:, seen_dac:, path:)
+    path = NetworkPath.new(node, path)
+
+    return 0 if seen.include?(path)
+
+    seen << path
+
+    cached(path) do
+      case node
+      when :fft
+        seen_fft = true
+      when :dac
+        seen_dac = true
+      end
+
+      destinations = network[node]
+
+      destinations.map do |destination|
+        if destination == end_at
+          if seen_fft && seen_dac
+            puts "found a path!!"
+            1
+          else
+            0
+          end
+        else
+          path_count_from(destination, seen_fft:, seen_dac:, path:)
+        end
+      end.sum
+    end
+  end
+end
