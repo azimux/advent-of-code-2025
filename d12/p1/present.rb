2b6814f3d317a9c702cf994c64b40f096f6732a6
Miles Georgi <azimux@gmail.com>
2026-03-25 16:58:57 -0700

    Add Present#flip and #rotate

--- d12/p1/present.rb
+++ d12/p1/present.rb
@@ -5,4 +5,22 @@ class Present
     self.index = index
     self.spaces = spaces
   end
+
+  def flip
+    Present.new(index, spaces.map(&:reverse))
+  end
+
+  def rotate
+    rotated_spaces = (0...width).map do |column|
+      spaces.map { |row| row[column] }.reverse.join
+    end
+
+    Present.new(index, rotated_spaces)
+  end
+
+  def width = spaces.first.size
+
+  def to_s
+    spaces.join("\n")
+  end
 end
